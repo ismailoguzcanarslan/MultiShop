a19254bd59c356b93645e09e56609700e6146d36
ismailoguzcanarslan <ismailoguzcanarslan@gmail.com>
2024-12-29 20:29:49 +0300

    Add project files.

---
+++ Services/Catalog/MultiShop.Catalog/Services/CategoryService/CategoryService.cs
@@ -0,0 +1,44 @@
+﻿using AutoMapper;
+using MongoDB.Driver;
+using MultiShop.Catalog.Dtos.CategoryDtos;
+using MultiShop.Catalog.Entities;
+
+namespace MultiShop.Catalog.Services.CategoryService
+{
+    public class CategoryService : ICategoryService
+    {
+        private readonly IMongoCollection<Category> _categories;
+        private readonly IMapper _mapper;
+
+        public CategoryService(IMongoCollection<Category> categories, IMapper mapper)
+        {
+            _categories = categories;
+            _mapper = mapper;
+        }
+
+        public Task CreateCategoryAsync(CreateCategoryDto categoryDto)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task DeleteCategoryAsync(string id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<List<ResultCategoryDto>> GetAllAsync()
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<GetByIdCategoryDto> GetByIdCategoryAsync(string id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task UpdateCategoryAsync(UpdateCategoryDto categoryDto)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
---
+++ Services/Catalog/MultiShop.Catalog/Services/CategoryService/ICategoryService.cs
@@ -0,0 +1,14 @@
+﻿using MultiShop.Catalog.Dtos.CategoryDtos;
+
+namespace MultiShop.Catalog.Services.CategoryService
+{
+    public interface ICategoryService
+    {
+        Task<List<ResultCategoryDto>> GetAllAsync();
+        Task CreateCategoryAsync(CreateCategoryDto categoryDto);
+        Task UpdateCategoryAsync(UpdateCategoryDto categoryDto);
+        Task DeleteCategoryAsync(string id);
+        Task<GetByIdCategoryDto> GetByIdCategoryAsync(string id);
+        
+    }
+}
